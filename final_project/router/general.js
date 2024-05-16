e9fa5cb0689400a1f7ea25ea2680ab52a78e8548
Bashore <spamme853@gmail.com>
2024-05-16 13:31:01 -0400

    Post promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,43 +31,53 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
+let bookPromise = new Promise((resolve,reject) => {
+  // A timeout to fake a delay in database lookup
+  setTimeout(() => {
+    resolve(books)
+  }, 1000)})
 
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send( JSON.stringify(books) );
+  bookPromise.then((promise_books) =>
+    res.send( JSON.stringify(promise_books) ));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  res.send( JSON.stringify(books[isbn]));
-//  return res.status(300).json({message: "Yet to be implemented"});
- });
+  bookPromise.then((promise_books) =>
+    res.send( JSON.stringify(promise_books[isbn]) ));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
 
   const author = req.params.author;
   let author_books = [];
-  for(let book in books) {
-    if (books[book].author === author) {
-      author_books.push(books[book]);
-    }
-  };
-  res.send(author_books);
+  bookPromise.then((promise_books) => {
+    for(let book in promise_books) {
+      if (promise_books[book].author === author) {
+        author_books.push(promise_books[book]);
+      }
+    };
+    res.send(author_books);
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
   let ret_books = [];
-  for(let book in books) {
-    if (books[book].title === title) {
-      ret_books.push(books[book]);
-    }
-  };
-  res.send(ret_books);
+  bookPromise.then((promise_books) => {
+    for(let book in promise_books) {
+      if (promise_books[book].title === title) {
+        ret_books.push(promise_books[book]);
+      }
+    };
+    res.send(ret_books);
+  });
 });
 
 //  Get book review
