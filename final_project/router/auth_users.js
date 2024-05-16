91a107c26174634889eabb8b9fcce47a5ebabd58
Bashore <spamme853@gmail.com>
2024-05-16 13:11:01 -0400

    Pre promise

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,21 +9,74 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 });
+
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let reviews = books[req.params.isbn].reviews;
+  let user = req.session.authorization.username;
+  let review = req.body.review;
+  console.log("isbn " + req.params.isbn + " user " + user + " reviews " + reviews);
+  if (reviews[user]) {
+    console.log("Review exists");
+    reviews[user] = review;
+    return res.status(200).send("Review updated. The current reviews for this book are: " + 
+      JSON.stringify(reviews));
+  } else {
+    console.log("New Review ");
+    reviews[user] = review;
+    return res.status(200).send("Review added.  The current reviews for this book are: " + 
+      JSON.stringify(reviews));
+  }
+
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+
+  let reviews = books[req.params.isbn].reviews;
+  let user = req.session.authorization.username;
+  if (reviews[user]) {
+    delete reviews[user];
+    return res.status(200).send("Review deleted.  The current reviews for this book are: " + 
+      JSON.stringify(reviews));
+  } else {
+    return res.status(200).send("You have not reviewed this book.  The current reviews for this book are: " + 
+      JSON.stringify(reviews));
+  }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
